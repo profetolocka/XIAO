0bad60341499d36ba31846ffc6bf08201b4b1173
profe Tolo <ernesto_tolocka@yahoo.com.ar>
2024-12-01 12:24:13 -0300

    Update SensorTHBitmaps.py

--- Español/C3-Base-OLED-DHT11-Bitmap-Mpy/SensorTHBitmaps.py
+++ Español/C3-Base-OLED-DHT11-Bitmap-Mpy/SensorTHBitmaps.py
@@ -3,11 +3,24 @@
 # Usa fonts grandes y bitmaps
 
 #Equivalencias entre nombres de pines y GPIOs
+D0 = 2
+D1 = 3
+D2 = 4
+D3 = 5
+D4 = 6
 D5 = 7
+D6 = 21
+D7 = 20
+D8 = 8
+D9 = 9
+D10 = 10
+
 
 from time import sleep
 from machine import Pin, SoftI2C 
 from ssd1306 import Display
+from xglcd_font import XglcdFont
+
 from dht import DHT11
 
 #Crear objeto I2C
@@ -19,13 +32,31 @@ display = Display(i2c=i2c, width=128, height=64)
 #Crear objeto sensor
 sensorTH = DHT11 (Pin(D5))
 
+# Carga font
+perfect = XglcdFont('fonts/PerfectPixel_23x32.c', 23, 32)
+
 
 while (True):
     
     sensorTH.measure ()  #Mide Temperatura y humedad del DHT11
     
-    print (sensorTH.temperature(),"grados")
-    print (sensorTH.humidity (),"%")
+    temp = sensorTH.temperature()
+    hum  = sensorTH.humidity()
+    
+    print (temp,"grados")
+    print (hum ,"%")
+    
+    tempStr = f"{temp:.1f}"
+    humStr  = f"{hum:.0f}"
+
+    #display.draw_text(5, 10, "Temp        Hum", robotron, True)
+
+
+    display.draw_text(5, 30, tempStr, perfect, False)
+    display.draw_text(80, 30, humStr, perfect, False)
+    
+    display.present()
+
 
     #Mostrar bitmap
     #display.draw_bitmap("images/seeedLogo.mono", 0, 0, 116, 64)
