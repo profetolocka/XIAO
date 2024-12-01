b621cc6771d831fe81516a8483bc40be624ff88a
profe Tolo <ernesto_tolocka@yahoo.com.ar>
2024-12-01 12:04:53 -0300

    Update SensorTHBitmaps.py

--- Español/C3-Base-OLED-DHT11-Bitmap-Mpy/SensorTHBitmaps.py
+++ Español/C3-Base-OLED-DHT11-Bitmap-Mpy/SensorTHBitmaps.py
@@ -2,9 +2,13 @@
 # Muestra valores en OLED de placa de expansión
 # Usa fonts grandes y bitmaps
 
+#Equivalencias entre nombres de pines y GPIOs
+D5 = 7
+
 from time import sleep
 from machine import Pin, SoftI2C 
 from ssd1306 import Display
+from dht import DHT11
 
 #Crear objeto I2C
 i2c = SoftI2C(freq=400000, scl=Pin(7), sda=Pin(6))  #Pines de la XIAO ESP32-C3
@@ -12,7 +16,16 @@ i2c = SoftI2C(freq=400000, scl=Pin(7), sda=Pin(6))  #Pines de la XIAO ESP32-C3
 #Crear objeto display
 display = Display(i2c=i2c, width=128, height=64)
 
+#Crear objeto sensor
+sensorTH = DHT11 (Pin(D5))
+
+
 while (True):
+    
+    sensorTH.measure ()  #Mide Temperatura y humedad del DHT11
+    
+    print (sensorTH.temperature(),"grados")
+    print (sensorTH.humidity (),"%")
 
     #Mostrar bitmap
     #display.draw_bitmap("images/seeedLogo.mono", 0, 0, 116, 64)
@@ -20,9 +33,9 @@ while (True):
     #sleep(10)
 
     #Borrar buffer
-    display.clear_buffers()
+    #display.clear_buffers()
 
     #Mostrar bitmap
-    display.draw_bitmap("images/seeedLogo.mono", 0, 0, 116, 64, invert=True)
-    display.present()
+    #display.draw_bitmap("images/seeedLogo.mono", 0, 0, 116, 64, invert=True)
+    #display.present()
     sleep(10)
