76416daeab1bccc9f9c2a6870be96d16060d6010
profe Tolo <ernesto_tolocka@yahoo.com.ar>
2024-12-07 13:09:54 -0300

    Adapta a RP2040

--- Español/RP2040-Base-OLED-Bitmap-Mpy/demoBitmapLogoRP2040.py
+++ Español/RP2040-Base-OLED-Bitmap-Mpy/demoBitmapLogoRP2040.py
@@ -1,11 +1,25 @@
 #Demo de imagen bitmap. Muestra el logo de Seeed Studio
+#Usa una placa XIAO RP2040
+
+#Pines
+D0 = 26
+D1 = 27
+D2 = 28
+D3 = 29
+D4 = 6
+D5 = 7
+D6 = 0
+D7 = 1
+D8 = 2
+D9 = 4
+D10 = 3
 
 from time import sleep
 from machine import Pin, SoftI2C 
 from ssd1306 import Display
 
 #Crear objeto I2C
-i2c = SoftI2C(freq=400000, scl=Pin(7), sda=Pin(6))  #Pines de la XIAO ESP32-C3
+i2c = SoftI2C(freq=400000, scl=Pin(D5), sda=Pin(D4))  #Pines de la XIAO RP2040
 
 #Crear objeto display
 display = Display(i2c=i2c, width=128, height=64)
