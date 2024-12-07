20c2f7078b7333d8dbd2526c18688adb9b04d33a
profe Tolo <ernesto_tolocka@yahoo.com.ar>
2024-12-07 18:09:48 -0300

    Agrega Buzzer

--- Español/RP2040-Base-Oled-DHT11-Mpy/MuestraTH.py
+++ Español/RP2040-Base-Oled-DHT11-Mpy/MuestraTH.py
@@ -19,11 +19,16 @@ D10 = 3
 SDA_PIN = D4
 SCL_PIN = D5
 
-from machine import Pin, SoftI2C
+#Buzzer
+BUZZER_PIN = D3
+
+from machine import Pin, SoftI2C, PWM
 from time import sleep
 from dht import DHT11
 from ssd1306 import SSD1306_I2C #Libreria oficial
 
+#Crea objeto Buzzer como PWM. DC inicial = 0
+Buzzer = PWM(Pin(BUZZER_PIN), freq=1000, duty_u16=0)
 
 #Crea objeto I2C
 i2c = SoftI2C(scl=Pin(SCL_PIN), sda=Pin(SDA_PIN), freq=100000)  #Inicializa i2c
@@ -31,9 +36,17 @@ i2c = SoftI2C(scl=Pin(SCL_PIN), sda=Pin(SDA_PIN), freq=100000)  #Inicializa i2c
 #Crea objeto oled
 Display = SSD1306_I2C (128, 64, i2c)
 
-
 #El modulo Grove DHT11 está conectado a D7
 sensorTH = DHT11 (Pin(D7))
+
+# Hace un bip usando el zumbador pasivo de la placa de expansión
+def Beep ():
+    Buzzer.duty_u16(32768)     # Fija DC a 50%
+    sleep (0.1)
+    Buzzer.duty_u16(0)         # Fija DC a 0%
+
+    
+# Loop
 
 while (1):
     sensorTH.measure ()  #Mide
@@ -48,6 +61,8 @@ while (1):
     Display.text ("Hum=",10,20,1)
     Display.text (str(sensorTH.humidity ()),50,20,1)
     Display.show()
+    
+    Beep ()
     
     sleep (5)
 
