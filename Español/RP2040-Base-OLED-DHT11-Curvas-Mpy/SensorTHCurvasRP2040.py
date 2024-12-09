880bc62e75ffb9a8f93ccfdb88ef90a8f1878b18
profe Tolo <ernesto_tolocka@yahoo.com.ar>
2024-12-09 19:12:22 -0300

    Agrega Modo

--- Español/RP2040-Base-OLED-DHT11-Curvas-Mpy/SensorTHCurvasRP2040.py
+++ Español/RP2040-Base-OLED-DHT11-Curvas-Mpy/SensorTHCurvasRP2040.py
@@ -37,14 +37,25 @@ DHT11_PIN = D7
 #Buzzer
 BUZZER_PIN = D3
 
+#Botón
+BUTTON_PIN = D1
+
 # Cantidad de valores a graficar
 maxValues = 100
 
 # Tiempo entre mediciones
-sampleTime = 2
+sampleTime = 1
+
+# Modos de visualizacion
+modes = ["Values", "PlotTemp", "PlotHum"]
 
 ####################  Funciones ###################
 
+def readButton ():
+    # Lee el User Button de la placa de expansion en D1
+    button = Pin (D1, Pin.IN, Pin.PULL_UP)
+    return (button.value())
+    
 def beep():
     # Hace un beep en el buzzer pasivo
     buzzer = PWM(Pin(BUZZER_PIN))
@@ -59,12 +70,32 @@ def showList ():
     for i in listTH:
         print (i)
         
+def showTH (temp, hum):
+    # Muestra TyH con bitmaps
+    tempStr = f"{temp:.1f}"
+    humStr  = f"{hum:.0f}"
+
+    #Borra display
+    #display.clear()
+
+    #Imprime valores con numeros grandes
+    display.draw_text(5, 31, tempStr, perfect, False)
+    display.draw_text(85, 31, humStr, perfect, False)
+    
+    #Mostrar bitmaps
+    display.draw_bitmap("images/TempIcon.mono", 25, 0, 32, 32, True)
+    display.draw_bitmap("images/HumIcon.mono",  85, 0, 32, 32, True)
+
+    #Actualizar pantalla
+    display.present()
+    
+
 def plotHum ():
     # Grafica los valores de humedad
     
     maxHum = 100
     
-    display.clear()
+    #display.clear()
     display.draw_rectangle(20, 4, 104, 56, invert=False)
     #Ventana para imprimir 100 x 54
     
@@ -74,12 +105,13 @@ def plotHum ():
     display.draw_text(10, 54, "0", fixed, False)
     
     #Titulo
-    display.draw_text(58, 5, "Hum", fixed, False)
+    display.draw_text(5, 15, "H", fixed, False)
 
     #Plotear los valores almacenados
     x = 22
     for i in listTH:
         h = int (i[1]*56/maxHum)  #Escalar
+        display.draw_vline(x, 5, 56-h, invert=True)
         display.draw_vline(x, 60-h, h, invert=False)
         x=x+1
     
@@ -90,7 +122,7 @@ def plotTemp ():
     
     maxTemp = 50
     
-    display.clear()
+    #display.clear()
     display.draw_rectangle(20, 4, 104, 56, invert=False)
     #Ventana para imprimir 100 x 54
     
@@ -100,8 +132,8 @@ def plotTemp ():
     display.draw_text(10, 54, "0", fixed, False)
     
     #Titulo
-    display.draw_text(58, 5, "Temp", fixed, False)
-
+    display.draw_text(5, 15, "T", fixed, False)
+    
     #Plotear los valores almacenados
     x = 22
     for i in listTH:
@@ -129,6 +161,8 @@ fixed   = XglcdFont('fonts/FixedFont5x8.c',5,8)
 # Crear objeto lista para guardar los valores para el gráfico
 listTH = deque ([], maxValues)  #No usa maxlen=
 
+# Arranca mostrando valores
+modeIndex = 0
 
 # Loop
 while (True):
@@ -136,7 +170,7 @@ while (True):
     # Medir temperatura y humedad
     sensorTH.measure ()
      
-    beep ()
+    #beep ()
     
     # Separar valores
     temp = sensorTH.temperature()
@@ -144,32 +178,26 @@ while (True):
     
     # Guardarlo en la lista (deque)
     listTH.append ((temp,hum))
-    
-    # Muestra los valores almacenados
-    showList () 
   
-# Imprimir por consola
+    # Imprimir por consola
     print (temp,"grados")
     print (hum ,"%")
     
-    tempStr = f"{temp:.1f}"
-    humStr  = f"{hum:.0f}"
+    if (readButton()==0):
+        beep ()
+        modeIndex = (modeIndex + 1) % len (modes)
+        display.clear()
 
-    #Borrar buffer
-    display.clear_buffers()
-    
-    plotHum ()
-    
-    #Mostrar valores de temperatura y humedad en OLED
-    #display.draw_text(5, 31, tempStr, perfect, False)
-    #display.draw_text(85, 31, humStr, perfect, False)
-    
-    #Mostrar bitmaps
-    #display.draw_bitmap("images/TempIcon.mono", 25, 0, 32, 32, True)
-    #display.draw_bitmap("images/HumIcon.mono",  85, 0, 32, 32, True)
+    if (modes[modeIndex] == "Values"):
+        showTH (temp, hum)
+    elif (modes[modeIndex] == "PlotTemp"):
+        plotTemp ()
+    else:
+        plotHum ()
+            
+   
 
-    #Actualizar pantalla
-    display.present()
+    #print (button.value())
 
     # Esperar para la próxima medición
     sleep(sampleTime)
