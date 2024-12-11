02914417996e1ec8b57c28623ce383260f4abe49
profe Tolo <ernesto_tolocka@yahoo.com.ar>
2024-12-11 12:22:48 -0300

    Created

---
+++ English/C3-Base-OLED-DHT11-Bitmap-Mpy/README.md
@@ -0,0 +1,30 @@
+# Sensor de temperatura y humedad con DHT11
+
+# Descripción
+Este proyecto consiste en un sensor de temperatura y humedad basado en la placa **XIAO ESP32-C3** y el módulo **Grove DHT11**. El módulo se conecta a la Placa de expansión y los valores de temperatura y humedad se muestran en el display OLED.
+Se usan fonts grandes para los valores numéricos y se muestran imágenes bitmaps relacionadas con las magnitudes.
+
+![alt text](images/C3BaseOledDHT11Bitmaps.jpg)
+
+# Librerías
+Este proyecto usa la [librería de rdagger](https://github.com/rdagger/micropython-ssd1306) para controlar el OLED.
+Hay que instalar la librería del OLED (ssd1306.py) y la que permite el manejo de fonts (xglcd_font.py). Se lo puede hacer manualmente o usando MIP:
+
+```python annotate
+>>> import network
+>>> wlan=network.WLAN (network.STA_IF)
+>>> wlan.active (True)
+True
+>>> wlan.connect ("xxxx", "xxxx")
+>>> import mip
+>>> mip.install ("https://raw.githubusercontent.com/rdagger/micropython-ssd1306/refs/heads/main/ssd1306.py")
+Downloading https://raw.githubusercontent.com/rdagger/micropython-ssd1306/refs/heads/main/ssd1306.py to /lib
+Copying: /lib/ssd1306.py
+Done
+>>> mip.install ("https://raw.githubusercontent.com/rdagger/micropython-ssd1306/refs/heads/main/xglcd_font.py")
+Downloading https://raw.githubusercontent.com/rdagger/micropython-ssd1306/refs/heads/main/xglcd_font.py to /lib
+Copying: /lib/xglcd_font.py
+Done
+>>> 
+
+```
---
+++ English/C3-Base-OLED-DHT11-Bitmap-Mpy/SensorTHBitmaps.py
@@ -0,0 +1,66 @@
+# Sensor de temperatura y humedad con modulo Grove DHT11
+# Muestra valores en OLED de placa de expansión
+# Usa fonts grandes y bitmaps
+
+#Equivalencias entre nombres de pines y GPIOs
+D0 = 2
+D1 = 3
+D2 = 4
+D3 = 5
+D4 = 6
+D5 = 7
+D6 = 21
+D7 = 20
+D8 = 8
+D9 = 9
+D10 = 10
+
+
+from time import sleep
+from machine import Pin, SoftI2C 
+from ssd1306 import Display
+from xglcd_font import XglcdFont
+
+from dht import DHT11
+
+#Crear objeto I2C
+i2c = SoftI2C(freq=400000, scl=Pin(7), sda=Pin(6))  #Pines de la XIAO ESP32-C3
+
+#Crear objeto display
+display = Display(i2c=i2c, width=128, height=64)
+
+#Crear objeto sensor
+sensorTH = DHT11 (Pin(D5))
+
+# Carga font grande
+perfect = XglcdFont('fonts/PerfectPixel_23x32.c', 23, 32)
+
+
+while (True):
+    
+    sensorTH.measure ()  #Mide Temperatura y humedad del DHT11
+    
+    temp = sensorTH.temperature()
+    hum  = sensorTH.humidity()
+    
+    print (temp,"grados")
+    print (hum ,"%")
+    
+    tempStr = f"{temp:.1f}"
+    humStr  = f"{hum:.0f}"
+
+    #Borrar buffer
+    display.clear_buffers()
+    
+    #Mostrar valores de temperatura y humedad en OLED
+    display.draw_text(5, 31, tempStr, perfect, False)
+    display.draw_text(85, 31, humStr, perfect, False)
+    
+    #Mostrar bitmaps
+    display.draw_bitmap("images/TempIcon.mono", 25, 0, 32, 32, True)
+    display.draw_bitmap("images/HumIcon.mono",  85, 0, 32, 32, True)
+
+    #Actualizar pantalla
+    display.present()
+
+    sleep(10)
