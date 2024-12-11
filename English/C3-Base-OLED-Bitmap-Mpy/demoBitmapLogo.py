8120ffc485fab071125f70561f8a9a807526797b
profe Tolo <ernesto_tolocka@yahoo.com.ar>
2024-12-11 12:21:38 -0300

    Comments updated

--- English/C3-Base-OLED-Bitmap-Mpy/demoBitmapLogo.py
+++ English/C3-Base-OLED-Bitmap-Mpy/demoBitmapLogo.py
@@ -1,26 +1,26 @@
-#Demo de imagen bitmap. Muestra el logo de Seeed Studio
+#Bitmap image demo. Displays the Seeed Studio logo
 
 from time import sleep
 from machine import Pin, SoftI2C 
 from ssd1306 import Display
 
-#Crear objeto I2C
-i2c = SoftI2C(freq=400000, scl=Pin(7), sda=Pin(6))  #Pines de la XIAO ESP32-C3
+#Create I2C object
+i2c = SoftI2C(freq=400000, scl=Pin(7), sda=Pin(6))  #Pins for the XIAO ESP32-C3
 
-#Crear objeto display
+#Create display object
 display = Display(i2c=i2c, width=128, height=64)
 
 while (True):
 
-    #Mostrar bitmap
+    #Display bitmap
     display.draw_bitmap("images/seeedLogo.mono", 0, 0, 116, 64)
     display.present()
     sleep(10)
 
-    #Borrar buffer
+    #Clear buffer
     display.clear_buffers()
 
-    #Mostrar bitmap
+    #Display bitmap
     display.draw_bitmap("images/seeedLogo.mono", 0, 0, 116, 64, invert=True)
     display.present()
     sleep(10)
