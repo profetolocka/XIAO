fd4a771f2aa0045aa56016c975d59ad14dd1127c
profe Tolo <ernesto_tolocka@yahoo.com.ar>
2024-12-13 15:53:15 -0300

    DHT11 en D7

--- Español/C3-Base-Oled-DHT11-Mpy/MuestraTH.py
+++ Español/C3-Base-Oled-DHT11-Mpy/MuestraTH.py
@@ -1,8 +1,18 @@
 # Mide valores de temperatura y humedad usando el módulo Grove DHT11
 # Muestra en OLED de base de expansion usando lib ssd1306 oficial
 
-# Pines
+# Pines y equivalencias con GPIO
+D0 = 2
+D1 = 3
+D2 = 4
+D3 = 5
+D4 = 6
 D5 = 7
+D6 = 21
+D7 = 20
+D8 = 8
+D9 = 9
+D10 = 10
 
 from machine import Pin, SoftI2C
 from time import sleep
@@ -11,13 +21,13 @@ from ssd1306 import SSD1306_I2C #Libreria oficial
 
 
 #Crea objeto I2C
-i2c = SoftI2C(scl=Pin(7), sda=Pin(6), freq=100000)  #Inicializa i2c
+i2c = SoftI2C(scl=Pin(D5), sda=Pin(D4), freq=100000)  #Inicializa i2c
 
 #Crea objeto oled
 Display = SSD1306_I2C (128, 64, i2c)
 
 
-sensorTH = DHT11 (Pin(D5))
+sensorTH = DHT11 (Pin(D7))
 
 while (1):
     sensorTH.measure ()  #Mide
