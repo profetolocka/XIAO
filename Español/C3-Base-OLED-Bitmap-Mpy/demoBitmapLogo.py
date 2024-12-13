310f25c760c7ce9ee5d43283ffcddf3fd3b8fd17
profe Tolo <ernesto_tolocka@yahoo.com.ar>
2024-12-13 15:02:51 -0300

    Agrega definiciones de pines

--- Español/C3-Base-OLED-Bitmap-Mpy/demoBitmapLogo.py
+++ Español/C3-Base-OLED-Bitmap-Mpy/demoBitmapLogo.py
@@ -1,11 +1,24 @@
 #Demo de imagen bitmap. Muestra el logo de Seeed Studio
 
+#Equivalencias entre nombres de pines y GPIOs
+D0 = 2
+D1 = 3
+D2 = 4
+D3 = 5
+D4 = 6
+D5 = 7
+D6 = 21
+D7 = 20
+D8 = 8
+D9 = 9
+D10 = 10
+
 from time import sleep
 from machine import Pin, SoftI2C 
 from ssd1306 import Display
 
 #Crear objeto I2C
-i2c = SoftI2C(freq=400000, scl=Pin(7), sda=Pin(6))  #Pines de la XIAO ESP32-C3
+i2c = SoftI2C(freq=400000, scl=Pin(D5), sda=Pin(D4))  #Pines de la XIAO ESP32-C3
 
 #Crear objeto display
 display = Display(i2c=i2c, width=128, height=64)
