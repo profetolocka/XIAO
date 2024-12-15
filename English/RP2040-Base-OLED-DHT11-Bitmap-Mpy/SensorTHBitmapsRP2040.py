b93483b0d047ac1660014daa2a5eff199381104e
profe Tolo <profetolocka@gmail.com>
2024-12-15 09:39:29 -0300

    Comments updated

--- English/RP2040-Base-OLED-DHT11-Bitmap-Mpy/SensorTHBitmapsRP2040.py
+++ English/RP2040-Base-OLED-DHT11-Bitmap-Mpy/SensorTHBitmapsRP2040.py
@@ -1,8 +1,11 @@
-# Sensor de temperatura y humedad con RP2040 y modulo Grove DHT11
-# Muestra valores en OLED de placa de expansión
-# Usa fonts grandes y bitmaps
+# Author: Ernesto Tolocka (profe Tolocka)
+# Date: 15-12-2024
+# Description: Temperature and humidity sensor with RP2040 and Grove DHT11 module
+# 	Displays values on OLED of expansion board
+# 	Uses large fonts and bitmaps
+# License: MIT
 
-#Equivalencias entre nombres de pines y GPIOs
+# Pin name and GPIO mappings
 D0 = 26
 D1 = 27
 D2 = 28
@@ -15,60 +18,58 @@ D8 = 2
 D9 = 4
 D10 = 3
 
-# Pines de I2C de la RP2040
+# RP2040 I2C pins
 PIN_SDA = D4
 PIN_SCL = D5
 
-# Pin de conexion del DHT11
+# DHT11 connection pin
 PIN_DHT11 = D7
 
 from time import sleep
 from machine import Pin, SoftI2C 
 from ssd1306 import Display
 from xglcd_font import XglcdFont
-
-
 from dht import DHT11
 
-#Crear objeto I2C
+# Create I2C object
 i2c = SoftI2C(freq=400000, scl=Pin(PIN_SCL), sda=Pin(PIN_SDA)) 
 
-#Crear objeto display
+# Create display object
 display = Display(i2c=i2c, width=128, height=64)
 
-#Crear objeto sensor
-sensorTH = DHT11 (Pin(PIN_DHT11))
+# Create sensor object
+sensorTH = DHT11(Pin(PIN_DHT11))
 
-# Carga font grande
+# Load large font
 perfect = XglcdFont('fonts/PerfectPixel_23x32.c', 23, 32)
-
 
 # Loop
 while (True):
     
-    sensorTH.measure ()  #Mide Temperatura y humedad del DHT11
+    sensorTH.measure()  # Measure temperature and humidity from the DHT11
     
     temp = sensorTH.temperature()
     hum  = sensorTH.humidity()
     
-    print (temp,"grados")
-    print (hum ,"%")
+    print(temp, "degrees")
+    print(hum, "%")
     
     tempStr = f"{temp:.1f}"
     humStr  = f"{hum:.0f}"
 
-    #Borrar buffer
+    # Clear buffer
     display.clear_buffers()
     
-    #Mostrar valores de temperatura y humedad en OLED
+    # Display temperature and humidity values on OLED
     display.draw_text(5, 31, tempStr, perfect, False)
     display.draw_text(85, 31, humStr, perfect, False)
     
-    #Mostrar bitmaps
+    # Display bitmaps
     display.draw_bitmap("images/TempIcon.mono", 25, 0, 32, 32, True)
     display.draw_bitmap("images/HumIcon.mono",  85, 0, 32, 32, True)
 
-    #Actualizar pantalla
+    # Update screen
     display.present()
 
     sleep(10)
+    
